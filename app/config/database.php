a89cfe703fcf0ff7b59b31f4d2a078618fe7934a
Andy Awidarto <andy.awidarto@gmail.com>
2015-08-28 15:58:22 +0700

    initial

--- app/config/database.php
+++ app/config/database.php
@@ -48,7 +48,7 @@ return array(
 
         'mongodb' => array(
             'driver'   => 'mongodb',
-            'host'     => 'localhost',
+            'host'     => '127.0.0.1',
             'port'     => 27017,
             'username' => 'jayonexpress',
             'password' => 'bosseddy234',
