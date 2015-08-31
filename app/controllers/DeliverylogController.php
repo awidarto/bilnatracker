5ea68e4e2a0f2e25da1465eeb5760bcfca5977c3
Andy Awidarto <andy.awidarto@gmail.com>
2015-08-31 16:39:54 +0700

    controllers

--- app/controllers/DeliverylogController.php
+++ app/controllers/DeliverylogController.php
@@ -96,13 +96,13 @@ class DeliverylogController extends AdminController {
             array('Zone',array('search'=>true,'sort'=>true)),
             array('City',array('search'=>true,'sort'=>true)),
             array('Shipping Address',array('search'=>true,'sort'=>true, 'style'=>'max-width:200px;width:200px;' )),
-            array('No Kode Penjualan Toko',array('search'=>true,'sort'=>true)),
+            array('No Invoice',array('search'=>true,'sort'=>true)),
             array('Type',array('search'=>true,'sort'=>true,'select'=>Config::get('jayon.deliverytype_selector_legacy') )),
             array('Merchant & Shop Name',array('search'=>true,'sort'=>true)),
-            array('Delivery ID',array('search'=>true,'sort'=>true)),
+            array('Box ID',array('search'=>true,'sort'=>true)),
             array('Status',array('search'=>true,'sort'=>true)),
             array('Directions',array('search'=>true,'sort'=>true)),
-            array('TTD Toko',array('search'=>true,'sort'=>true)),
+            array('Signatures',array('search'=>true,'sort'=>true)),
             array('Delivery Charge',array('search'=>true,'sort'=>true)),
             array('COD Surcharge',array('search'=>true,'sort'=>true)),
             array('COD Value',array('search'=>true,'sort'=>true)),
@@ -201,13 +201,13 @@ class DeliverylogController extends AdminController {
             array('Zone',array('search'=>true,'sort'=>true)),
             array('City',array('search'=>true,'sort'=>true)),
             array('Shipping Address',array('search'=>true,'sort'=>true, 'style'=>'max-width:200px;width:200px;' )),
-            array('No Kode Penjualan Toko',array('search'=>true,'sort'=>true)),
+            array('No Invoice',array('search'=>true,'sort'=>true)),
             array('Type',array('search'=>true,'sort'=>true)),
             array('Merchant & Shop Name',array('search'=>true,'sort'=>true)),
-            array('Delivery ID',array('search'=>true,'sort'=>true)),
+            array('Box ID',array('search'=>true,'sort'=>true)),
             array('Status',array('search'=>true,'sort'=>true)),
             array('Directions',array('search'=>true,'sort'=>true)),
-            array('TTD Toko',array('search'=>true,'sort'=>true)),
+            array('Signatures',array('search'=>true,'sort'=>true)),
             array('Delivery Charge',array('search'=>true,'sort'=>true)),
             array('COD Surcharge',array('search'=>true,'sort'=>true)),
             array('COD Value',array('search'=>true,'sort'=>true)),
@@ -236,30 +236,23 @@ class DeliverylogController extends AdminController {
         //table generator part
 
         $this->fields = array(
+            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
             array('ordertime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickuptime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
+            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverytime',array('kind'=>'daterange','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliveryslot',array('kind'=>'text' , 'query'=>'like', 'pos'=>'both','show'=>true)),
             array('buyerdeliveryzone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverycity',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('shipping_address',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
-            array('merchant_trans_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('delivery_type',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+            array('fulfillment_code',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+            array('merchant_trans_id',array('kind'=>'text','callback'=>'dispBar' ,'query'=>'like','pos'=>'both','show'=>true)),
+            array('delivery_type',array('kind'=>'text','callback'=>'colorizetype' ,'query'=>'like','pos'=>'both','show'=>true)),
             array('directions',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('delivery_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('cod_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('total_price',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('buyer_name',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('shipping_zip',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('phone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('volume',array('kind'=>'numeric','query'=>'like','pos'=>'both','show'=>true)),
-            array('weight',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
         );
 
         $db = Config::get('jayon.main_db');
@@ -282,22 +275,24 @@ class DeliverylogController extends AdminController {
     {
 
         $this->heads = array(
+            array('Logistic & Fee Scheme',array('search'=>true,'sort'=>true)),
             array('Timestamp',array('search'=>true,'sort'=>true, 'style'=>'min-width:90px;','daterange'=>true)),
             array('PU Time',array('search'=>true,'sort'=>true, 'style'=>'min-width:100px;','daterange'=>true)),
             array('PU Pic',array('search'=>true,'sort'=>true, 'style'=>'min-width:120px;')),
             array('PU Person & Device',array('search'=>true,'style'=>'min-width:100px;','sort'=>true)),
+            array('Status',array('search'=>true,'sort'=>true)),
             array('Delivery Date',array('search'=>true,'style'=>'min-width:125px;','sort'=>true, 'daterange'=>true )),
             array('Slot',array('search'=>true,'sort'=>true)),
             array('Zone',array('search'=>true,'sort'=>true)),
             array('City',array('search'=>true,'sort'=>true)),
             array('Shipping Address',array('search'=>true,'sort'=>true, 'style'=>'max-width:200px;width:200px;' )),
-            array('No Kode Penjualan Toko',array('search'=>true,'sort'=>true)),
-            array('Type',array('search'=>true,'sort'=>true,'select'=>Config::get('jayon.deliverytype_selector') )),
-            array('Merchant & Shop Name',array('search'=>true,'sort'=>true)),
-            array('Delivery ID',array('search'=>true,'sort'=>true)),
-            array('Status',array('search'=>true,'sort'=>true)),
+            array('Box ID',array('search'=>true,'sort'=>true)),
+            array('Fulfillment ID',array('search'=>true,'sort'=>true)),
+            array('No Invoice',array('search'=>true,'sort'=>true)),
+            array('Type',array('search'=>true,'sort'=>true,'select'=>Config::get('jayon.deliverytype_selector_legacy') )),
             array('Directions',array('search'=>true,'sort'=>true)),
-            array('TTD Toko',array('search'=>true,'sort'=>true)),
+            array('Signatures',array('search'=>true,'sort'=>true)),
+/*
             array('Delivery Charge',array('search'=>true,'sort'=>true)),
             array('COD Surcharge',array('search'=>true,'sort'=>true)),
             array('COD Value',array('search'=>true,'sort'=>true)),
@@ -306,6 +301,8 @@ class DeliverylogController extends AdminController {
             array('Phone',array('search'=>true,'sort'=>true)),
             array('W x H x L = V',array('search'=>true,'sort'=>true)),
             array('Weight Range',array('search'=>true,'sort'=>true)),
+*/
+
         );
 
         //print $this->model->where('docFormat','picture')->get()->toJSON();
@@ -325,22 +322,23 @@ class DeliverylogController extends AdminController {
         //table generator part
 
         $this->fields = array(
+            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
             array('ordertime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickuptime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
+            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverytime',array('kind'=>'daterange','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliveryslot',array('kind'=>'text' , 'query'=>'like', 'pos'=>'both','show'=>true)),
             array('buyerdeliveryzone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverycity',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('shipping_address',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
-            array('merchant_trans_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('delivery_type',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
+            array('merchant_trans_id',array('kind'=>'text','callback'=>'dispBar' ,'query'=>'like','pos'=>'both','show'=>true)),
+            array('delivery_type',array('kind'=>'text','callback'=>'colorizetype' ,'query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('directions',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+/*
             array('delivery_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('cod_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('total_price',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
@@ -349,6 +347,7 @@ class DeliverylogController extends AdminController {
             array('phone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('volume',array('kind'=>'numeric','query'=>'like','pos'=>'both','show'=>true)),
             array('weight',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+*/
         );
 
         $db = Config::get('jayon.main_db');
--- app/controllers/ZoningController.php
+++ app/controllers/ZoningController.php
@@ -87,30 +87,23 @@ class ZoningController extends AdminController {
 
 
         $this->heads = array(
+            array('Logistic & Fee Scheme',array('search'=>true,'sort'=>true)),
             array('Timestamp',array('search'=>true,'sort'=>true, 'style'=>'min-width:90px;','daterange'=>true)),
             array('PU Time',array('search'=>true,'sort'=>true, 'style'=>'min-width:100px;','daterange'=>true)),
             array('PU Pic',array('search'=>true,'sort'=>true, 'style'=>'min-width:120px;')),
             array('PU Person & Device',array('search'=>true,'style'=>'min-width:100px;','sort'=>true)),
+            array('Status',array('search'=>true,'sort'=>true)),
             array('Delivery Date',array('search'=>true,'style'=>'min-width:125px;','sort'=>true, 'daterange'=>true )),
             array('Slot',array('search'=>true,'sort'=>true)),
             array('Zone',array('search'=>true,'sort'=>true)),
             array('City',array('search'=>true,'sort'=>true)),
             array('Shipping Address',array('search'=>true,'sort'=>true, 'style'=>'max-width:200px;width:200px;' )),
-            array('No Kode Penjualan Toko',array('search'=>true,'sort'=>true)),
+            array('Box ID',array('search'=>true,'sort'=>true)),
+            array('Fulfillment ID',array('search'=>true,'sort'=>true)),
+            array('No Invoice',array('search'=>true,'sort'=>true)),
             array('Type',array('search'=>true,'sort'=>true,'select'=>Config::get('jayon.deliverytype_selector_legacy') )),
-            array('Merchant & Shop Name',array('search'=>true,'sort'=>true)),
-            array('Delivery ID',array('search'=>true,'sort'=>true)),
-            array('Status',array('search'=>true,'sort'=>true)),
             array('Directions',array('search'=>true,'sort'=>true)),
-            array('TTD Toko',array('search'=>true,'sort'=>true)),
-            array('Delivery Charge',array('search'=>true,'sort'=>true)),
-            array('COD Surcharge',array('search'=>true,'sort'=>true)),
-            array('COD Value',array('search'=>true,'sort'=>true)),
-            array('Buyer',array('search'=>true,'sort'=>true)),
-            array('ZIP',array('search'=>true,'sort'=>true)),
-            array('Phone',array('search'=>true,'sort'=>true)),
-            array('W x H x L = V',array('search'=>true,'sort'=>true)),
-            array('Weight Range',array('search'=>true,'sort'=>true)),
+            array('Signatures',array('search'=>true,'sort'=>true)),
         );
 
         //print $this->model->where('docFormat','picture')->get()->toJSON();
@@ -141,30 +134,24 @@ class ZoningController extends AdminController {
     {
 
         $this->fields = array(
+            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
             array('ordertime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickuptime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
+            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverytime',array('kind'=>'daterange','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliveryslot',array('kind'=>'text' , 'query'=>'like', 'pos'=>'both','show'=>true)),
             array('buyerdeliveryzone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverycity',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('shipping_address',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
+            array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+            array('fulfillment_code',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('merchant_trans_id',array('kind'=>'text','callback'=>'dispBar' ,'query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_type',array('kind'=>'text','callback'=>'colorizetype' ,'query'=>'like','pos'=>'both','show'=>true)),
-            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
-            array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('directions',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('delivery_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('cod_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('total_price',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('buyer_name',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('shipping_zip',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('phone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('volume',array('kind'=>'numeric','query'=>'like','pos'=>'both','show'=>true)),
-            array('weight',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+
         );
 
         /*
@@ -201,13 +188,13 @@ class ZoningController extends AdminController {
             array('Zone',array('search'=>true,'sort'=>true)),
             array('City',array('search'=>true,'sort'=>true)),
             array('Shipping Address',array('search'=>true,'sort'=>true, 'style'=>'max-width:200px;width:200px;' )),
-            array('No Kode Penjualan Toko',array('search'=>true,'sort'=>true)),
+            array('No Invoice',array('search'=>true,'sort'=>true)),
             array('Type',array('search'=>true,'sort'=>true)),
             array('Merchant & Shop Name',array('search'=>true,'sort'=>true)),
-            array('Delivery ID',array('search'=>true,'sort'=>true)),
+            array('Box ID',array('search'=>true,'sort'=>true)),
             array('Status',array('search'=>true,'sort'=>true)),
             array('Directions',array('search'=>true,'sort'=>true)),
-            array('TTD Toko',array('search'=>true,'sort'=>true)),
+            array('Signatures',array('search'=>true,'sort'=>true)),
             array('Delivery Charge',array('search'=>true,'sort'=>true)),
             array('COD Surcharge',array('search'=>true,'sort'=>true)),
             array('COD Value',array('search'=>true,'sort'=>true)),
@@ -282,22 +269,23 @@ class ZoningController extends AdminController {
     {
 
         $this->heads = array(
+            array('Logistic & Fee Scheme',array('search'=>true,'sort'=>true)),
             array('Timestamp',array('search'=>true,'sort'=>true, 'style'=>'min-width:90px;','daterange'=>true)),
             array('PU Time',array('search'=>true,'sort'=>true, 'style'=>'min-width:100px;','daterange'=>true)),
             array('PU Pic',array('search'=>true,'sort'=>true, 'style'=>'min-width:120px;')),
             array('PU Person & Device',array('search'=>true,'style'=>'min-width:100px;','sort'=>true)),
+            array('Status',array('search'=>true,'sort'=>true)),
             array('Delivery Date',array('search'=>true,'style'=>'min-width:125px;','sort'=>true, 'daterange'=>true )),
             array('Slot',array('search'=>true,'sort'=>true)),
             array('Zone',array('search'=>true,'sort'=>true)),
             array('City',array('search'=>true,'sort'=>true)),
             array('Shipping Address',array('search'=>true,'sort'=>true, 'style'=>'max-width:200px;width:200px;' )),
-            array('No Kode Penjualan Toko',array('search'=>true,'sort'=>true)),
-            array('Type',array('search'=>true,'sort'=>true,'select'=>Config::get('jayon.deliverytype_selector') )),
-            array('Merchant & Shop Name',array('search'=>true,'sort'=>true)),
-            array('Delivery ID',array('search'=>true,'sort'=>true)),
-            array('Status',array('search'=>true,'sort'=>true)),
+            array('No Invoice',array('search'=>true,'sort'=>true)),
+            array('Type',array('search'=>true,'sort'=>true,'select'=>Config::get('jayon.deliverytype_selector_legacy') )),
+            array('Box ID',array('search'=>true,'sort'=>true)),
             array('Directions',array('search'=>true,'sort'=>true)),
-            array('TTD Toko',array('search'=>true,'sort'=>true)),
+            array('Signatures',array('search'=>true,'sort'=>true)),
+/*
             array('Delivery Charge',array('search'=>true,'sort'=>true)),
             array('COD Surcharge',array('search'=>true,'sort'=>true)),
             array('COD Value',array('search'=>true,'sort'=>true)),
@@ -306,6 +294,8 @@ class ZoningController extends AdminController {
             array('Phone',array('search'=>true,'sort'=>true)),
             array('W x H x L = V',array('search'=>true,'sort'=>true)),
             array('Weight Range',array('search'=>true,'sort'=>true)),
+*/
+
         );
 
         //print $this->model->where('docFormat','picture')->get()->toJSON();
@@ -325,22 +315,23 @@ class ZoningController extends AdminController {
         //table generator part
 
         $this->fields = array(
+            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
             array('ordertime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickuptime',array('kind'=>'daterange', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('pickup_person',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
+            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverytime',array('kind'=>'daterange','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliveryslot',array('kind'=>'text' , 'query'=>'like', 'pos'=>'both','show'=>true)),
             array('buyerdeliveryzone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('buyerdeliverycity',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
             array('shipping_address',array('kind'=>'text', 'query'=>'like','pos'=>'both','show'=>true)),
-            array('merchant_trans_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('delivery_type',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array(Config::get('jayon.jayon_members_table').'.merchantname',array('kind'=>'text','alias'=>'merchant_name','query'=>'like','callback'=>'merchantInfo','pos'=>'both','show'=>true)),
+            array('merchant_trans_id',array('kind'=>'text','callback'=>'dispBar' ,'query'=>'like','pos'=>'both','show'=>true)),
+            array('delivery_type',array('kind'=>'text','callback'=>'colorizetype' ,'query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('directions',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('delivery_id',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+/*
             array('delivery_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('cod_cost',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('total_price',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
@@ -349,6 +340,7 @@ class ZoningController extends AdminController {
             array('phone',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('volume',array('kind'=>'numeric','query'=>'like','pos'=>'both','show'=>true)),
             array('weight',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+*/
         );
 
         $db = Config::get('jayon.main_db');
