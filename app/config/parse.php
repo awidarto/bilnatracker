2a8750c5ee9db913cf926cd5b528d515a7122c80
Andy Awidarto <andy.awidarto@gmail.com>
2015-09-24 19:39:15 +0700

    refresh

--- app/config/parse.php
+++ app/config/parse.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-        'app_id'=>'lNz2h3vr3eJK9QMAKOLSaIvETaQWsbFJ8Em32TIw',
-        'client_key'=>'GlHlKA3HUVSKPFGdzccRRu63xL8ZYyuXbxkksoLY',
-        'rest_key'=>'8QQoPiTZTkqSMkYLQQxHiaKBXO6Jq7iD2dCJjGUz',
-        'master_key'=>'2bKlPqYIKMpW1rJOdpBXQ8pf7cMXxGaFKrCXMr19'
+        'app_id'=>'edB6kEzIHsGXOkGZPPXoUvxDlKJXIhfMprizPJpt',
+        'client_key'=>'5R1SwY06NtRTtzGsYBdjQGTcyom247qJugg5qRQ0',
+        'rest_key'=>'f4uJhZekscDFxrRdANBzQq5a0gloM089MOcHEmZK',
+        'master_key'=>'g6ffsrOpdkfdbuiSnHdr5ysgK66zmE0nvt2mPfKT'
     );
