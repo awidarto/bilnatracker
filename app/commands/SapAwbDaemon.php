e2f13897346bb47481a4f721afa1d0183c85c017
Andy Awidarto <andy.awidarto@gmail.com>
2015-12-05 09:50:20 +0700

    sap

--- app/commands/SapAwbDaemon.php
+++ app/commands/SapAwbDaemon.php
@@ -88,14 +88,15 @@ class SapAwbDaemon extends Command {
 
                         //print $data_string;
 
-                        $url = $base_url.'?key='.$logistic->api_key;
+                        $url = $base_url;
+                        //.'?key='.$logistic->api_key;
 
                         $ch = curl_init($url);
                         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
                         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
                         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
                         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                            'Content-Type: application/json',
+                            'Content-Type: text/xml',
                             'Content-Length: ' . strlen($data_string))
                         );
 
