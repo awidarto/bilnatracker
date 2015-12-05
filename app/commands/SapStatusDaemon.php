42a5ad3eced1a50ac81573b6c7ba68731899e8d0
Andy Awidarto <andy.awidarto@gmail.com>
2015-12-05 20:28:03 +0700

    sap

--- app/commands/SapStatusDaemon.php
+++ app/commands/SapStatusDaemon.php
@@ -37,7 +37,105 @@ class SapStatusDaemon extends Command {
 	 */
 	public function fire()
 	{
-		//
+
+        //$base_url = 'http://localhost/jexadmin/public/api/v1/service/status';
+        $base_url = 'http://182.23.64.223/serverapi.sap/api/tracking/list/id/';
+        $logistic_id = 'CGKN00284';
+
+        $delivery_trigger = 'delivered';
+        $returned_trigger = 'returned';
+
+        $logistic = Logistic::where('consignee_olshop_cust','=',$logistic_id)->first();
+
+        $orders = Shipment::where('awb','!=','')
+                        ->where('bucket','=',Config::get('jayon.bucket_tracker'))
+                        ->where('status','!=','delivered')
+                        ->where('logistic_type','=','external')
+                        ->where('consignee_olshop_cust','=',$logistic_id)
+                        ->get();
+
+        if($orders && count($orders->toArray()) > 0){
+            $req = array();
+            foreach($orders as $ord){
+                //$req[] = array('awb'=>$ord->awb);
+                $client = new GuzzleClient();
+
+                $url = $base_url.$ord->awb;
+                $request = $client->get($url, array());
+
+                $request->setAuth('5490188', '5351');
+
+                $response = $request->send();
+
+                print $response->getBody();
+            }
+
+            /*
+            $awblist = json_decode($response->getBody());
+
+
+            $awbs = array();
+            $ffs = array();
+            foreach ($awblist as $awb) {
+                $awbarray[] = trim($awb->awb);
+                $awbs[$awb->awb] = $awb;
+            }
+
+            $orderlist = Shipment::whereIn('awb', $awbarray)->get();
+
+            foreach($orderlist as $order){
+
+                $pre = clone $order;
+
+                if( $awbs[$order->awb]->status == $delivery_trigger){
+                    $order->status = Config::get('jayon.trans_status_mobile_delivered');
+                    $order->position = 'CUSTOMER';
+                }
+
+                if($awbs[$order->awb]->status == $returned_trigger){
+                    $order->status = Config::get('jayon.trans_status_mobile_return');
+                }
+
+                $order->district = $awbs[$order->awb]->district;
+                $order->logistic_status = $awbs[$order->awb]->status;
+                $order->logistic_status_ts = $awbs[$order->awb]->timestamp;
+                $order->logistic_raw_status = $awbs[$order->awb];
+                $order->save();
+
+                $ts = new MongoDate();
+
+                $hdata = array();
+                $hdata['historyTimestamp'] = $ts;
+                $hdata['historyAction'] = 'api_shipment_change_status';
+                $hdata['historySequence'] = 1;
+                $hdata['historyObjectType'] = 'shipment';
+                $hdata['historyObject'] = $order->toArray();
+                $hdata['actor'] = $this->name;
+                $hdata['actor_id'] = '';
+
+                History::insert($hdata);
+
+                $sdata = array();
+                $sdata['timestamp'] = $ts;
+                $sdata['action'] = 'api_shipment_change_status';
+                $sdata['reason'] = 'api_update';
+                $sdata['objectType'] = 'shipment';
+                $sdata['object'] = $order->toArray();
+                $sdata['preObject'] = $pre->toArray();
+                $sdata['actor'] = $this->name;
+                $sdata['actor_id'] = '';
+                Shipmentlog::insert($sdata);
+
+            }
+
+            */
+
+        }else{
+            print 'Empty order list'."\r\n";
+        }
+
+        $actor = $this->name;
+        Event::fire('log.api',array('SapStatusDaemon', 'get' ,$actor,'SAP STATUS PULL'));
 	}
 
 	/**
