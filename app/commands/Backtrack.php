5d0a286126dbe61c2bee091ea086d80ed4540063
Andy Awidarto <andy.awidarto@gmail.com>
2015-12-13 09:57:54 +0700

    pu date bilna

--- app/commands/Backtrack.php
+++ app/commands/Backtrack.php
@@ -43,6 +43,7 @@ class Backtrack extends Command {
 
         if($dbox){
             foreach($dbox as $dbx){
+                print 'box status : '.$dbx->deliveryId.' '.$dbx->dbxId.' '.$box->deliveryStatus."\r\n";
 
                 $box = Box::where('delivery_id','=',$dbx->deliveryId)
                                 ->where(function($q) use($dbx){
