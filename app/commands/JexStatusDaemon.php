2db5108b868f9ca422eb006e0dfe7e9cb1b214b1
Andy Awidarto <andy.awidarto@gmail.com>
2015-12-26 08:25:59 +0700

    no log

--- app/commands/JexStatusDaemon.php
+++ app/commands/JexStatusDaemon.php
@@ -134,15 +134,15 @@ class JexStatusDaemon extends Command {
                 $hdata['actor'] = $this->name;
                 $hdata['actor_id'] = '';
 
-                History::insert($hdata);
+                //History::insert($hdata);
 
                 $sdata = array();
                 $sdata['timestamp'] = $ts;
                 $sdata['action'] = 'api_shipment_change_status';
                 $sdata['reason'] = 'api_update';
                 $sdata['objectType'] = 'shipment';
-                $sdata['object'] = $order->toArray();
-                $sdata['preObject'] = $pre->toArray();
+                $sdata['object'] = $order;
+                $sdata['preObject'] = $pre;
                 $sdata['actor'] = $this->name;
                 $sdata['actor_id'] = '';
                 Shipmentlog::insert($sdata);
