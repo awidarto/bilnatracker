a66eba6fcfdfa59997e762cb01cc9cc9a44fafdc
Andy Awidarto <andy.awidarto@gmail.com>
2016-02-11 19:46:31 +0700

    grouping

--- app/config/cs.php
+++ app/config/cs.php
@@ -4,8 +4,8 @@ return array(
         '7735'=>array('status'=>'multi',
                         'order'=>'ts',
                         'awb'=>'awb',
-                        'group'=>array('status','timestamp'),
-                        'get'=>array('status','timestamp','pickup_time','delivery_time','pod','note')
+                        'group'=>array('timestamp','status'),
+                        'get'=>array('timestamp','status','pickup_time','delivery_time','pod','note')
                     ),
         'default'=>array('status'=>'multi',
                         'order'=>'ts',
