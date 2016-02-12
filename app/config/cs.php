969ad742f4b95435cfbdd4aae19216457891829c
Andy Awidarto <andy.awidarto@gmail.com>
2016-02-12 14:07:32 +0700

    grouping

--- app/config/cs.php
+++ app/config/cs.php
@@ -3,13 +3,15 @@
 return array(
         'CGKN00284'=>array('status'=>'multi',
                         'order'=>'ts',
-                        'awb'=>'cn_no',
+                        'awb'=>'cn_ref',
+                        'order_awb'=>'fulfillment_code',
                         'group'=>array(),
                         'get'=>array()
                     ),
         '7735'=>array('status'=>'multi',
                         'order'=>'ts',
                         'awb'=>'awb',
+                        'order_awb'=>'awb',
                         'group'=>array(),
                         'get'=>array('timestamp','status','pickup_time','delivery_time','pod','note')
                     ),
