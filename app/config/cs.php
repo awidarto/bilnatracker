1f637aab056a22f4ebe5af4132eba19d8598a401
Andy Awidarto <andy.awidarto@gmail.com>
2016-02-12 13:51:13 +0700

    grouping

--- app/config/cs.php
+++ app/config/cs.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-        'CGKN00284'=>array('status'=>'single',
+        'CGKN00284'=>array('status'=>'multi',
                         'order'=>'ts',
                         'awb'=>'cn_no',
                         'group'=>array(),
